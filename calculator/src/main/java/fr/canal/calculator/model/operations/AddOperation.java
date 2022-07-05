5dea8955d4726d298197039d4f2497c757af9b01
Rima Chamakh <chamakhrima96@gmail.com>
2022-07-05 15:55:07 +0100

    correct the add operator

--- calculator/src/main/java/fr/canal/calculator/model/operations/AddOperation.java
+++ calculator/src/main/java/fr/canal/calculator/model/operations/AddOperation.java
@@ -11,7 +11,7 @@ public class AddOperation extends Operation{
             OperationResult result = new OperationResult();
 
             if(this.getOperand1() != null && this.getOperand2() != null) {
-                result.setResult(this.getOperand1().subtract(this.getOperand2()));
+                result.setResult(this.getOperand1().add(this.getOperand2()));
                 result.setError(Boolean.FALSE);
             }else {
                 result.setError(Boolean.TRUE);
